c7f9fb5b47ed61f919449ad9d5bd1cb663e963c4
Omar Farag <omarfarag74@gmail.com>
2021-03-31 23:42:28 -0400

    Added basic initialization for VGA buffers, basic draw line and plot pixel functions

--- duckHunt.c
+++ duckHunt.c
@@ -1,1 +1,172 @@
+/* This files provides address values that exist in the system */
+
+#define SDRAM_BASE            0xC0000000
+#define FPGA_ONCHIP_BASE      0xC8000000
+#define FPGA_CHAR_BASE        0xC9000000
+
+/* Cyclone V FPGA devices */
+#define LEDR_BASE             0xFF200000
+#define HEX3_HEX0_BASE        0xFF200020
+#define HEX5_HEX4_BASE        0xFF200030
+#define SW_BASE               0xFF200040
+#define KEY_BASE              0xFF200050
+#define TIMER_BASE            0xFF202000
+#define PIXEL_BUF_CTRL_BASE   0xFF203020
+#define CHAR_BUF_CTRL_BASE    0xFF203030
+
+/* VGA colors */
+#define WHITE 0xFFFF
+#define YELLOW 0xFFE0
+#define RED 0xF800
+#define GREEN 0x07E0
+#define BLUE 0x001F
+#define CYAN 0x07FF
+#define MAGENTA 0xF81F
+#define GREY 0xC618
+#define PINK 0xFC18
+#define ORANGE 0xFC00
+
+#define ABS(x) (((x) > 0) ? (x) : -(x))
+
+/* Screen size. */
+#define RESOLUTION_X 320
+#define RESOLUTION_Y 240
+
+/* Constants for animation */
+#define BOX_LEN 4
+#define NUM_BOXES 8
+
+#define FALSE 0
+#define TRUE 1
+
+#include <stdlib.h>
+#include <stdio.h>
+
+// Begin part3.c code for Lab 7
+
+volatile int pixel_buffer_start; // global variable	
+	
+void plot_pixel(int x, int y, short int line_color)
+{
+    *(short int *)(pixel_buffer_start + (y << 10) + (x << 1)) = line_color;
+}
+	
+
+	
+int abs(int a) {
+	if (a < 0) return -a;
+	return a;
+}
+
+void swap (int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void draw_line(int x1, int y1, int x2, int y2, short int color) {
+	int is_steep;// = abs(y2 - y1) > abs(x2 - x1)? 1: 0;
+	if (abs(y2 - y1) > abs(x2 - x1)) {
+		is_steep = 1;
+	} else {
+		is_steep = 0;
+	}
+	if (is_steep) {
+		swap(&x1, &y1);
+		swap(&x2, &y2);
+	}
+	if (x1 > x2) {
+		swap(&x1, &x2);
+		swap(&y1, &y2);
+		
+	}
+	int deltax = x2 - x1;
+	int deltay = abs(y2 - y1);
+	int error = -(deltax / 2);
+	int y = y1;
+	int y_step;// = y1 < y2 ? 1 : -1;
+	if (y1 < y2) {
+		y_step = 1;
+	} else {
+		y_step = -1;
+	}
+	
+	for (int i = x1; i < x2; i++) {
+		if (is_steep) {
+			plot_pixel(y, i, color);
+		} else {
+			plot_pixel(i, y, color);
+		}
+		error = error + deltay;
+		if (error >= 0) {
+			y += y_step;
+			error = error - deltax;
+		}
+	}
+}
+
+void clear_screen() {
+	for (int x = 0; x < 320; x++) {
+		for (int y = 0; y < 240; y++) {
+			*(short int *)(pixel_buffer_start + (y << 10) + (x << 1)) = 0x0;
+		}
+	}
+}
+
+void wait_for_vsync() {
+	volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
+	register int status;
+	*pixel_ctrl_ptr = 1;
+	
+	status = *(pixel_ctrl_ptr+3);
+	while ((status & 0x01) != 0) {
+		status = *(pixel_ctrl_ptr + 3);
+	}
+}	
+	
+
+
+volatile int pixel_buffer_start; // global variable
+
+int main(void)
+{
+	srand(time(0));
+    volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
+
+    /* set front pixel buffer to start of FPGA On-chip memory */
+    *(pixel_ctrl_ptr + 1) = 0xC8000000; // first store the address in the 
+                                        // back buffer
+    /* now, swap the front/back buffers, to set the front buffer location */
+    wait_for_vsync();
+    /* initialize a pointer to the pixel buffer, used by drawing functions */
+    pixel_buffer_start = *pixel_ctrl_ptr;
+    clear_screen(); // pixel_buffer_start points to the pixel buffer
+    /* set back pixel buffer to start of SDRAM memory */
+    *(pixel_ctrl_ptr + 1) = 0xC0000000;
+    pixel_buffer_start = *(pixel_ctrl_ptr + 1); // we draw on the back buffer
+    //Main loop
+	while (1)
+    {
+
+        clear_screen();
+
+
+		
+
+		
+
+
+
+
+		
+
+		wait_for_vsync();
+         // swap front and back buffers on VGA vertical sync
+        pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
+		
+    }
+	
+}
+
+// code for subroutines
 
