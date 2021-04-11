beba1920c56c2d57853a1c4d90211e398fe6c76b
ibrahim681 <81816993+ibrahim681@users.noreply.github.com>
2021-04-11 08:22:20 -0400

    Update game_screen.c
    included function that initializes the array robotImage and initRobot(robot* gameR, int t) which initializes the robot. Haven't included the render function similar to what is in duckHuck.c but there is a print function which prints what ever is in the robotImage array.

--- game_screen.c
+++ game_screen.c
@@ -491,4 +491,164 @@ void initilize_target_9x9(int x,int y){
 		else{biggerTarget[j+36].color = 0xffff; flip=false;}
 	}
 }
+/*******************************************************************/
+/********************Beginnig of Robot Code************************/
+/*******************************************************************/
+
+point robotImage[525];
+
+typedef struct robot {
+	int xPos;
+	int yPos;
+	int xVel;
+	int yVel;
+	int timer;
+	int shot; //1 if shot, 0 if not shot
+	int spawned; //Only draw and register shots when set to 1
+	point image[525];
+	point previous[525];
+	point toDelete[525];
+
+}robot;	
+	
+void initRobot(robot* gameR, int t) {
+	//Initialize Robot
+	
+	gameR->xPos = t%RESOLUTION_X;
+	gameR->yPos = t%RESOLUTION_Y;
+	robot_initHelper(gameR->xPos,gameR->yPos);
+	gameR->xVel = (t%2)*2 - 1;
+	gameR->yVel = (t%2)*2 - 1;
+	gameR->shot = 0;
+	gameR->timer = t%81 + 80;
+	for (int i = 0; i < 525; i++) {
+		gameR->image[i] = robotImage[i];
+		gameR->previous[i] = robotImage[i];
+		gameR->toDelete[i] = robotImage[i];
+	}
+}
+
+
+//this function fills the robotImage array at the location defined by (x,y) point
+void robot_initHelper(int x,int y){
+	//colummn x-10 and x+10
+	for(int i=0;i<25;i++){
+		if(i==14||i==15||i==16){robotImage[i].color = RED;robotImage[i+500].color = RED;}
+		else{robotImage[i].color = 0x0000;robotImage[i+500].color = 0x0000;}//->set to background color
+		robotImage[i].xPos = x-10;
+		robotImage[i].yPos = y-12+i;
+
+		robotImage[i+500].xPos = x+10;
+		robotImage[i+500].yPos = y-12+i;
+	}
+	//colummn x-9 and x+9 and x-8 and x+8
+	for(int i=0;i<25;i++){
+		if(i==8||i==9||i==10){robotImage[i+25].color = ROBO_BLUE;robotImage[i+475].color = ROBO_BLUE;robotImage[i+50].color = ROBO_BLUE;robotImage[i+450].color = ROBO_BLUE;}
+		else if(i==11||i==12||i==13){robotImage[i+25].color = GREY;robotImage[i+475].color = GREY;robotImage[i+50].color = GREY;robotImage[i+450].color = GREY;}
+		else if(i==14||i==15){robotImage[i+25].color = RED;robotImage[i+475].color = RED;robotImage[i+50].color = RED;robotImage[i+450].color = RED;}
+		else{robotImage[i+25].color = 0x0000;robotImage[i+475].color = 0x0000;robotImage[i+50].color = 0x0000;robotImage[i+450].color = 0x0000;}//->set to background color
+		robotImage[i+25].xPos = x-9;
+		robotImage[i+25].yPos = y-12+i;
+
+		robotImage[i+475].xPos = x+9;
+		robotImage[i+475].yPos = y-12+i;
+		//for column x-8 and x+8
+		robotImage[i+50].xPos = x-8;
+		robotImage[i+50].yPos = y-12+i;
+
+		robotImage[i+450].xPos = x+8;
+		robotImage[i+450].yPos = y-12+i;
+	}
+	//column x-7 and x+7
+	for(int i=0;i<25;i++){
+		if(i==8||i==9||i==10){robotImage[i+75].color = ROBO_BLUE;robotImage[i+425].color = ROBO_BLUE;}
+		else if(i==14||i==15||i==16){robotImage[i+75].color = RED;robotImage[i+425].color = RED;}
+		else{robotImage[i+75].color = 0x0000;robotImage[i+425].color = 0x0000;}//->set to background color
+		robotImage[i+75].xPos = x-7;
+		robotImage[i+75].yPos = y-12+i;
+
+		robotImage[i+425].xPos = x+7;
+		robotImage[i+425].yPos = y-12+i;
+	}
+	//column x-6 and x+6
+	for(int i=0;i<25;i++){
+		if(i>5&&i<18){robotImage[i+100].color = GREY;robotImage[i+400].color = GREY;}
+		else{robotImage[i+100].color = 0x0000;robotImage[i+400].color = 0x0000;}//->set to background color
+		robotImage[i+100].xPos = x-6;
+		robotImage[i+100].yPos = y-12+i;
+
+		robotImage[i+400].xPos = x+6;
+		robotImage[i+400].yPos = y-12+i;
+	}
+	//column x-5 and x-5
+	for(int i=0;i<25;i++){
+		if(i>5&&i<18){robotImage[i+125].color = GREY;robotImage[i+375].color = GREY;}
+		else if(i==23||i==24){robotImage[i+125].color = RED;robotImage[i+375].color = RED;}
+		else{robotImage[i+125].color = 0x0000;robotImage[i+375].color = 0x0000;}//->set to background color
+		robotImage[i+125].xPos = x-5;
+		robotImage[i+125].yPos = y-12+i;
+
+		robotImage[i+375].xPos = x+5;
+		robotImage[i+375].yPos = y-12+i;
+	}
+	//column x-4 and x+4
+	for(int i=0;i<25;i++){
+		if(i>5&&i<23){robotImage[i+150].color = GREY;robotImage[i+350].color = GREY;}
+		else if(i==2||i==3||i==23||i==24){robotImage[i+150].color = RED;robotImage[i+350].color = RED;}
+		else{robotImage[i+125].color = 0x0000;robotImage[i+350].color = 0x0000;}//->set to background color
+		robotImage[i+150].xPos = x-4;
+		robotImage[i+150].yPos = y-12+i;
+
+		robotImage[i+350].xPos = x+4;
+		robotImage[i+350].yPos = y-12+i;
+	}
+	//column x-3 and x+3
+	for(int i=0;i<25;i++){
+		if(i==23||i==24){robotImage[i+175].color = RED;robotImage[i+325].color = RED;}
+		else {robotImage[i+175].color = GREY;robotImage[i+325].color = GREY;}
+		robotImage[i+175].xPos = x-3;
+		robotImage[i+175].yPos = y-12+i;
+
+		robotImage[i+325].xPos = x+3;
+		robotImage[i+325].yPos = y-12+i;
+	}
+	//column x-2 and x+2
+	for(int i=0;i<25;i++){
+		if(i==23||i==24){robotImage[i+200].color = RED;robotImage[i+300].color = RED;}
+		else if(i==1){robotImage[i+200].color = WHITE;robotImage[i+300].color = 0x0000;}
+		else if(i==2){robotImage[i+200].color = 0x0000;robotImage[i+300].color = 0x0000;}
+		else {robotImage[i+200].color = GREY;robotImage[i+300].color = GREY;}
+		robotImage[i+200].xPos = x-2;
+		robotImage[i+200].yPos = y-12+i;
+
+		robotImage[i+300].xPos = x+2;
+		robotImage[i+300].yPos = y-12+i;
+	}
+	//column x-1 and x+1
+	for(int i=0;i<25;i++){
+		if(i==0||i==3|| i>4&&i<18){robotImage[i+225].color = GREY;robotImage[i+275].color = GREY;}
+		else if(i==4){robotImage[i+225].color = WHITE;robotImage[i+275].color = WHITE;}
+		else if(i==1){robotImage[i+225].color = 0x0000;robotImage[i+275].color = WHITE;}
+		else{robotImage[i+225].color = 0x0000;robotImage[i+275].color = 0x0000;}//->set to background color
+		robotImage[i+225].xPos = x-1;
+		robotImage[i+225].yPos = y-12+i;
+		
+		robotImage[i+275].xPos = x+1;
+		robotImage[i+275].yPos = y-12+i;
+	}
+	//column x
+	for(int i=0;i<25;i++){
+		if(i<4 || i>4&&i<18){robotImage[i+250].color = GREY;}
+		else if(i==4){robotImage[i+250].color = WHITE;}
+		else{robotImage[i+250].color = 0x0000;}//->set to background color
+		robotImage[i+250].xPos = x;
+		robotImage[i+250].yPos = y-12+i;
+	}
+}
+
+void print_robot(){
+	for(int i=0;i<525;i++){
+		draw_pixel(robotImage[i].xPos,robotImage[i].yPos,robotImage[i].color);
+	}	
+}
 
